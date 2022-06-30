7c35891e4876151c4a234750a29ad9ff471df8e5
Matheus Santos <somathsant@gmail.com>
2022-06-30 17:03:11 -0300

    Update insert

--- InsertCliente.sql
+++
@@ -1,3 +0,0 @@
-SELECT * from Cliente
-
-INSERT into Cliente (codcliente, email, nome, rg, rua, cidade, numrua) VALUES (001, 'antonio@gmail.com', 'Antonio Fonseca', 20077489008, 'rua idelfonso albano', 'Fortaleza', 477)
